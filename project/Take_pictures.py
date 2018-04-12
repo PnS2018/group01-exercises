0c3747b73fcf505d441acabe720af7057a6f8960
jbinz <jbinz@student.ethz.ch>
2018-04-12 12:06:58 +0000

    take_pic

--- project/Take_pictures.py
+++ project/Take_pictures.py
@@ -14,37 +14,74 @@ rawCapture = PiRGBArray(camera, size=(640, 480))
 
 # allow the camera to warmup
 time.sleep(0.1)
-i = 0
+f = open('Pi_Pictures/last_pic_index.txt', "r")
+i = int(f.read())
+f.close()
 for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
     # grab the raw NumPy array representing the image, then initialize the timestamp
     # and occupied/unoccupied text
     image = frame.array
-    if cv2.waitKey(1) & 0xFF == ord('1'):
-        string = 'Pi_Pictures/feature1/picture' + str(i) + '.png'
+    key = cv2.waitKey(100)
+    if key==ord('1'):
+        string = 'Pi_Pictures/Balls/picture' + str(i) + '.png'
         print(string)
         cv2.imwrite(string, image)
         i += 1
-    elif cv2.waitKey(1) & 0xFF == ord('2'):
-        string = 'Pi_Pictures/feature2/picture' + str(i) + '.png'
+    elif key==ord('2'):
+        string = 'Pi_Pictures/Bottles/picture' + str(i) + '.png'
+        print(string)
         cv2.imwrite(string, image)
         i += 1
-    elif cv2.waitKey(1) & 0xFF == ord('3'):
-        string = 'Pi_Pictures/feature3/picture' + str(i) + '.png'
+    elif key==ord('3'):
+        string = 'Pi_Pictures/Cans/picture' + str(i) + '.png'
+        print(string)
         cv2.imwrite(string, image)
         i += 1
-    elif cv2.waitKey(1) & 0xFF == ord('4'):
-        string = 'Pi_Pictures/feature4/picture' + str(i) + '.png'
+    elif key==ord('4'):
+        string = 'Pi_Pictures/Cups/picture' + str(i) + '.png'
+        print(string)
         cv2.imwrite(string, image)
         i += 1
-    elif cv2.waitKey(1) & 0xFF == ord('5'):
-        string = 'Pi_Pictures/feature5/picture' + str(i) + '.png'
+    elif key==ord('5'):
+        string = 'Pi_Pictures/Face/picture' + str(i) + '.png'
+        print(string)
         cv2.imwrite(string, image)
         i += 1
-    
-    cv2.imshow('frame', image)
+    elif key==ord('6'):
+        string = 'Pi_Pictures/Pens/picture' + str(i) + '.png'
+        print(string)
+        cv2.imwrite(string, image)
+        i += 1
+    elif key==ord('7'):
+        string = 'Pi_Pictures/Phone/picture' + str(i) + '.png'
+        print(string)
+        cv2.imwrite(string, image)
+        i += 1
+    elif key==ord('8'):
+        string = 'Pi_Pictures/Shoes/picture' + str(i) + '.png'
+        print(string)
+        cv2.imwrite(string, image)
+        i += 1
+    elif key==ord('9'):
+        string = 'Pi_Pictures/Silverware/picture' + str(i) + '.png'
+        print(string)
+        cv2.imwrite(string, image)
+        i += 1
+    elif key==ord('0'):
+        string = 'Pi_Pictures/Yoghurt/picture' + str(i) + '.png'
+        print(string)
+        cv2.imwrite(string, image)
+        i += 1
     # the loop breaks at pressing `q`
-    if cv2.waitKey(1) & 0xFF == ord('q'):
+    elif key==ord('q'):
+        string = 'Pi_Pictures/last_pic_index.txt'
+        f = open(string, "w")
+        f.write(str(i))
+        f.close()
         break
+    cv2.imshow('frame', image)
+
+    
 
     # clear the stream in preparation for the next frame
     rawCapture.truncate(0)
